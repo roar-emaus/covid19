168206070a381df1e8f54c44a5daf49637ece168
Roar Emaus <roar@emaus.no>
2021-03-16 07:05:44 +0100

    la til antall innlagte

--- covid19.py
+++ covid19.py
@@ -5,24 +5,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-df = pd.read_csv('surveillance_data/covid19/data_covid19_lab_by_time_latest.csv')
-df = df.set_index(pd.to_datetime(df['date']))
-#df = df['2021-02-01':'2021-03-10']
+start_date = '2020-11-01'
+end_date = None
+
+df_lab = pd.read_csv('surveillance_data/covid19/data_covid19_lab_by_time_latest.csv')
+df_lab = df_lab.set_index(pd.to_datetime(df_lab['date']))
+df_lab = df_lab[start_date:end_date]
 
 fig_npos_rel = plt.figure()
-ax = (df['n_pos']/(df['n_neg'] + df['n_pos'])).plot()
+ax = (df_lab['n_pos']/(df_lab['n_neg'] + df_lab['n_pos'])).plot()
 ax.set_ylabel(r'$\frac{\textbf{Positive}}{\textbf{Positive} + \textbf{Negative}}$')
 fig_npos_rel.axes.append(ax)
 
 fig_tot = plt.figure()
-ax2 = (df['n_pos']+df['n_neg']).plot()
+ax2 = (df_lab['n_pos']+df_lab['n_neg']).plot()
 ax2.set_ylabel(r'\textbf{Total}')
 fig_tot.axes.append(ax2)
 
 fig_npos = plt.figure()
-ax3 = df['n_pos'].plot()
+ax3 = df_lab['n_pos'].plot()
 ax3.set_ylabel(r'\textbf{Positive}')
 fig_npos.axes.append(ax3)
+
+df_hospital = pd.read_csv('surveillance_data/covid19/data_covid19_hospital_by_time_latest.csv')
+df_hospital = df_hospital.set_index(pd.to_datetime(df_hospital['date']))
+df_hospital = df_hospital[df_hospital['location_code'] == 'norge']
+df_hospital = df_hospital[start_date:end_date]
+fig_hospital = plt.figure()
+ax4 = df_hospital['n_icu'].plot(label='Intensiven')
+ax5 = df_hospital['n_hospital_main_cause'].plot(label='Hovedårsak')
+ax4.set_ylabel(r'\textbf{Innlagt}')
+fig_hospital.axes.append(ax4)
+fig_hospital.axes.append(ax5)
+plt.legend()
 
 plt.show()
 
