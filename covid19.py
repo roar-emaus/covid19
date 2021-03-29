ff91a2fd86cea1d66e895ec619fa1f495f9ce198
Roar Emaus <roar@emaus.no>
2021-03-29 13:18:01 +0200

    removed rubbish

--- covid19.py
+++ covid19.py
@@ -40,21 +40,5 @@ fig_hospital.axes.append(ax4)
 fig_hospital.axes.append(ax5)
 plt.legend()
 
-
-df = pd.read_csv('surveillance_data/covid19/data_covid19_msis_by_time_location_latest.csv')
-print(df['location_code'])
-oslo = df[df['location_code'] == 'county03']
-#mr = df[df['location_code'] == 'county15']
-aale = df[df['location_code'] == 'municip1507']
-print(aale)
-#mr_infected = [float(mr[mr['date'] == date]['n'] for date in sorted(set(df['date']))]
-#for date in sorted(set(df['date'])):
-#    print(date)
-#    print(aale[aale['date'] == date]['pr100000'])
-#aale_infected = [float(aale[aale['date'] == date]['pr100000']) for date in sorted(set(df['date']))]
-#oslo_infected = [float(oslo[oslo['date'] == date]['pr100000']) for date in sorted(set(df['date']))]
-
-#print(aale_infected)
-
-#plt.show()
+plt.show()
 
